e969eed79a513a895f1eb2b14c945da1b19724e1
pranav <pranav.techiegeek@gmail.com>
2025-11-13 18:36:24 -0700

    fix

---
+++ radio_rfm9x.py
@@ -0,0 +1,54 @@
+# SPDX-FileCopyrightText: 2018 Brent Rubell for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""
+Example for using the RFM9x Radio with Raspberry Pi.
+
+Learn Guide: https://learn.adafruit.com/lora-and-lorawan-for-raspberry-pi
+Author: Brent Rubell for Adafruit Industries
+"""
+# Import Python System Libraries
+import time
+# Import Blinka Libraries
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+import board
+
+# Import RFM9x
+import adafruit_rfm9x
+
+# Configure LoRa Radio
+CS = DigitalInOut(board.CE1)
+RESET = DigitalInOut(board.D25)
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
+
+# config
+rfm9x.signal_bandwidth = 125000
+rfm9x.coding_rate = 5
+rfm9x.spreading_factor = 9
+
+rfm9x.tx_power = 5
+prev_packet = None
+
+it = 0
+while True:
+    if it % 100 == 0:
+        rfm9x.send(bytes(f"Ping {it // 100}", "utf-8"))
+
+    packet = None
+    print('RasPi LoRa')
+
+    # check for packet rx
+    packet = rfm9x.receive()
+    if packet is None:
+        print('- Waiting for PKT -')
+    else:
+        # Display the packet text and rssi
+        prev_packet = packet
+        packet_text = str(prev_packet, "utf-8")
+        print('RX: ', packet_text)
+        time.sleep(1)
+
+    time.sleep(0.1)
