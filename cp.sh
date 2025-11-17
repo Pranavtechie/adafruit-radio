e54b878077989dc2eab5bbb86d278fd95e85f681
Pranav Mandava <pranav.techiegeek@gmail.com>
2025-11-17 11:05:28 -0700

    add circuit python

---
+++ cp.sh
@@ -0,0 +1,14 @@
+sudo apt-get install -y python3-pip
+sudo apt install --upgrade python3-setuptools 
+
+
+cd ~
+sudo apt install python3-venv
+python3 -m venv env --system-site-packages
+
+source env/bin/activate
+
+cd ~
+pip3 install --upgrade adafruit-python-shell
+wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
+sudo -E env PATH=$PATH python3 raspi-blinka.py
